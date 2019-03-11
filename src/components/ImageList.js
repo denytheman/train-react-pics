69254fce3c0df5a973cefd0df8a0b2d644cabc4d
Daniel Kovac <dkovac@logitech.com>
2019-03-11 11:07:41 -0700

    Creating image card component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './ImageList.css';
 
+import ImageCard from './ImageCard';
+
 const ImageList = props => {
     console.log(props.images);
 
-    const images = props.images.map(({ description, id, urls }) => {
-        return (
-            <img alt={description} key={id} src={urls.regular} />
-        )
+    const images = props.images.map(image => {
+        return <ImageCard key={image.id} image={image} />
     });
 
     return (
